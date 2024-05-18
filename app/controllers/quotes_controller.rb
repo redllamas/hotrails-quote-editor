a802e9c4994f9a328766d290796fa5374ef76f66
Sam Ellard <sam.ellard@gmail.com>
2024-05-18 11:02:32 +0900

    Added turbo Frames and Turbo Streams

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,31 +2,34 @@
 
 class QuotesController < ApplicationController
     before_action :set_quote, only: [:show, :edit, :update, :destroy]
-  
+
     def index
-      @quotes = Quote.all
+      @quotes = Quote.ordered
     end
-  
+
     def show
     end
-  
+
     def new
       @quote = Quote.new
     end
-  
+
     def create
       @quote = Quote.new(quote_params)
-  
+
       if @quote.save
-        redirect_to quotes_path, notice: "Quote was successfully created."
+        respond_to do |format|
+          format.html {redirect_to quotes_path, notice: "Quote was successfully created."}
+          format.turbo_stream
+        end
       else
-        render :new
+        render :new, status: :unprocessable_entity
       end
     end
-  
+
     def edit
     end
-  
+
     def update
       if @quote.update(quote_params)
         redirect_to quotes_path, notice: "Quote was successfully updated."
@@ -34,18 +37,21 @@ class QuotesController < ApplicationController
         render :edit
       end
     end
-  
+
     def destroy
       @quote.destroy
-      redirect_to quotes_path, notice: "Quote was successfully destroyed."
+      respond_to do |format|
+        format.html {redirect_to quotes_path, notice: "Quote was successfully destroyed."}
+        format.turbo_stream
+      end
     end
-  
+
     private
-  
+
     def set_quote
       @quote = Quote.find(params[:id])
     end
-  
+
     def quote_params
       params.require(:quote).permit(:name)
     end
