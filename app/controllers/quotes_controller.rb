b9bff22c6f68ad971a972214e0f801954f14865d
Sam Ellard <sam.ellard@gmail.com>
2024-05-20 16:35:20 +0900

    Added flash messages via Stimulus controller, plus an application helper to refactor partial render for multiple methods

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -20,7 +20,7 @@ class QuotesController < ApplicationController
       if @quote.save
         respond_to do |format|
           format.html {redirect_to quotes_path, notice: "Quote was successfully created."}
-          format.turbo_stream
+          format.turbo_stream {flash.now[:notice] = "Quote was successfully created." }
         end
       else
         render :new
@@ -32,9 +32,12 @@ class QuotesController < ApplicationController
 
     def update
       if @quote.update(quote_params)
-        redirect_to quotes_path, notice: "Quote was successfully updated."
+        respond_to do |format|
+          format.html {redirect_to quotes_path, notice: "Quote was successfully updated."}
+          format.turbo_stream { flash.now[:notice] = "Quote was successfully updated."}
+        end
       else
-        render :edit
+        render :edit, status: :unprocessable_entity
       end
     end
 
@@ -42,7 +45,7 @@ class QuotesController < ApplicationController
       @quote.destroy
       respond_to do |format|
         format.html {redirect_to quotes_path, notice: "Quote was successfully destroyed."}
-        format.turbo_stream
+        format.turbo_stream {flash.now[:notice] = "Quote was successfully destroyed."}
       end
     end
 
